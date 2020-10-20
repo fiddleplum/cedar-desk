c48e586c82e63d0bbd89d08a869ec78c20ff4681
Stephen Hurley <stephenhurley@gmail.com>
2020-10-20 05:09:15 -0700

    Added websocket connection.

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
-import { SimpleApp } from 'elm-app';
+import { SimpleApp, WS } from 'elm-app';
 
 export class CedarDeskApp extends SimpleApp {
 	/** Constructs the app. */
 	constructor() {
 		super();
 
+		console.log('here');
+
 		this.title('Cedar Desk');
+
+		this._ws = new WS('localhost:8081');
+		this._ws.getReadyPromise().then(() => {
+			this._ws.send({
+				test: 'test2'
+			});
+		});
 	}
 
 	/** Destructs the app. */
 	destroy(): void {
 	}
+
+	private _ws: WS;
 }
 
 // CedarDeskApp.html = /* html */`
@@ -18,9 +29,15 @@ export class CedarDeskApp extends SimpleApp {
 // 	<div id="menu">
 // 		<div id="title">
 // 			Cedar Desk
-// 		</div>	
+// 		</div>
 // 	</div>
 // 	`;
+
+CedarDeskApp.css = /* css */`
+	body {
+		background: black;
+		color: white;
+	}`;
 
 CedarDeskApp.setAppClass(CedarDeskApp);
 
