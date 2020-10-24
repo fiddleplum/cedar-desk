fd1112b7110a117856c9168990ad3ae77f192e96
Stephen Hurley <stephenhurley@gmail.com>
2020-10-24 11:44:59 -0700

    Beginning work on interface.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import { SimpleApp, WS } from 'elm-app';
+import { Cookies, App, WS } from 'elm-app';
 
-export class CedarDeskApp extends SimpleApp {
+export class CedarDeskApp extends App {
 	/** Constructs the app. */
 	constructor() {
 		super();
 
-		this.title('Cedar Desk');
+		this.element('title', HTMLDivElement).innerHTML = 'CedarDesk';
 
 		try {
 			this._ws = new WS('localhost:8081');
@@ -66,11 +66,66 @@ export class CedarDeskApp extends SimpleApp {
 	private _ws: WS;
 }
 
+CedarDeskApp.html = /* html */`
+	<div id="logo"><icon src="icon.svg"></icon></div>
+	<div id="title"></div>
+	<div id="menu"></div>
+	<div id="messages"></div>
+	<div id="page"></div>
+	<div id="toolbar"></div>
+	`;
+
 CedarDeskApp.css = /* css */`
 	body {
+		margin: 0;
+		height: 100%;
 		background: black;
 		color: white;
-	}`;
+		display: grid;
+		grid-template-rows: 3rem 2rem 1fr 2rem;
+		grid-template-columns: 3rem 1fr 1fr;
+		grid-template-areas:
+			"logo title menu"
+			"messages messages messages"
+			"page page page"
+			"toolbar toolbar toolbar";
+	}
+
+	.CedarDeskApp#logo {
+		grid-area: logo;
+		padding: .5rem;
+		line-height: 3rem;
+	}
+
+	.CedarDeskApp#title {
+		grid-area: title;
+		padding: .5rem;
+		font-size: 3rem;
+		line-height: 1.5rem;
+	}
+
+	.CedarDeskApp#menu {
+		grid-area: menu;
+	}
+
+	.CedarDeskApp#messages {
+		grid-area: messages;
+		font-size: 1.5rem;
+	}
+
+	.CedarDeskApp#page {
+		grid-area: page;
+	}
+
+	.CedarDeskApp#toolbar {
+		grid-area: toolbar;
+	}
+
+	svg {
+		width: 2rem;
+		height: 2rem;
+	}
+	`;
 
 CedarDeskApp.setAppClass(CedarDeskApp);
 
